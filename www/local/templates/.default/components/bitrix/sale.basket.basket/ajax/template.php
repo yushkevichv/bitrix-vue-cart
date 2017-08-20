078dc671bf4424c96c8def19289ba6356deb8905
Yushkevich Vitaly <yushkevichv@gmail.com>
2017-08-20 12:00:06 +0300

    init

---
+++ www/local/templates/.default/components/bitrix/sale.basket.basket/ajax/template.php
@@ -0,0 +1,106 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixBasketComponent $component */
+$templateData = array(
+	'TEMPLATE_THEME' => $this->GetFolder().'/themes/'.$arParams['TEMPLATE_THEME'].'/style.css',
+	'TEMPLATE_CLASS' => 'bx_'.$arParams['TEMPLATE_THEME'],
+);
+$this->addExternalCss($templateData['TEMPLATE_THEME']);
+
+$curPage = $APPLICATION->GetCurPage().'?'.$arParams["ACTION_VARIABLE"].'=';
+$arUrls = array(
+	"delete" => $curPage."delete&id=#ID#",
+	"delay" => $curPage."delay&id=#ID#",
+	"add" => $curPage."add&id=#ID#",
+);
+unset($curPage);
+
+$arParams['USE_ENHANCED_ECOMMERCE'] = isset($arParams['USE_ENHANCED_ECOMMERCE']) && $arParams['USE_ENHANCED_ECOMMERCE'] === 'Y' ? 'Y' : 'N';
+$arParams['DATA_LAYER_NAME'] = isset($arParams['DATA_LAYER_NAME']) ? trim($arParams['DATA_LAYER_NAME']) : 'dataLayer';
+$arParams['BRAND_PROPERTY'] = isset($arParams['BRAND_PROPERTY']) ? trim($arParams['BRAND_PROPERTY']) : '';
+
+$arBasketJSParams = array(
+	'SALE_DELETE' => GetMessage("SALE_DELETE"),
+	'SALE_DELAY' => GetMessage("SALE_DELAY"),
+	'SALE_TYPE' => GetMessage("SALE_TYPE"),
+	'TEMPLATE_FOLDER' => $templateFolder,
+	'DELETE_URL' => $arUrls["delete"],
+	'DELAY_URL' => $arUrls["delay"],
+	'ADD_URL' => $arUrls["add"],
+	'EVENT_ONCHANGE_ON_START' => (!empty($arResult['EVENT_ONCHANGE_ON_START']) && $arResult['EVENT_ONCHANGE_ON_START'] === 'Y') ? 'Y' : 'N',
+	'USE_ENHANCED_ECOMMERCE' => $arParams['USE_ENHANCED_ECOMMERCE'],
+	'DATA_LAYER_NAME' => $arParams['DATA_LAYER_NAME'],
+	'BRAND_PROPERTY' => $arParams['BRAND_PROPERTY']
+);
+?>
+
+<pre>
+    <? print_r(CUtil::PhpToJSObject($arBasketJSParams)); ?>
+</pre>
+
+<script type="text/javascript">
+	var basketJSParams = <?=CUtil::PhpToJSObject($arBasketJSParams);?>;
+</script>
+<?
+$APPLICATION->AddHeadScript($templateFolder."/script.js");
+
+
+if (strlen($arResult["ERROR_MESSAGE"]) <= 0)
+{
+	?>
+	<div id="warning_message">
+		<?
+		if (!empty($arResult["WARNING_MESSAGE"]) && is_array($arResult["WARNING_MESSAGE"]))
+		{
+			foreach ($arResult["WARNING_MESSAGE"] as $v)
+				ShowError($v);
+		}
+		?>
+	</div>
+	<?
+
+	$normalCount = count($arResult["ITEMS"]["AnDelCanBuy"]);
+	$normalHidden = ($normalCount == 0) ? 'style="display:none;"' : '';
+
+
+	foreach (array_keys($arResult['GRID']['HEADERS']) as $id)
+	{
+		$data = $arResult['GRID']['HEADERS'][$id];
+		$headerName = (isset($data['name']) ? (string)$data['name'] : '');
+		if ($headerName == '')
+			$arResult['GRID']['HEADERS'][$id]['name'] = GetMessage('SALE_'.$data['id']);
+		unset($headerName, $data);
+	}
+	unset($id);
+
+	?>
+		<form method="post" action="<?=POST_FORM_ACTION_URI?>" name="basket_form" id="basket_form">
+			<div id="basket_form_container">
+				<div class="bx_ordercart <?=$templateData['TEMPLATE_CLASS']; ?>">
+					<div class="bx_sort_container">
+						<span><?=GetMessage("SALE_ITEMS")?></span>
+						<a href="javascript:void(0)" id="basket_toolbar_button" class="current" onclick="showBasketItemsList()"><?=GetMessage("SALE_BASKET_ITEMS")?><div id="normal_count" class="flat" style="display:none">&nbsp;(<?=$normalCount?>)</div></a>
+					</div>
+					<?
+					include($_SERVER["DOCUMENT_ROOT"].$templateFolder."/basket_items.php");
+					?>
+				</div>
+			</div>
+			<input type="hidden" name="BasketOrder" value="BasketOrder" />
+			<!-- <input type="hidden" name="ajax_post" id="ajax_post" value="Y"> -->
+		</form>
+	<?
+}
+else
+{
+	ShowError($arResult["ERROR_MESSAGE"]);
+}
---
+++ www/urlrewrite.php
@@ -0,0 +1,41 @@
+<?
+$arUrlRewrite = array(
+	array(
+		"CONDITION" => "#^/bitrix/services/ymarket/#",
+		"RULE" => "",
+		"ID" => "",
+		"PATH" => "/bitrix/services/ymarket/index.php",
+	),
+	array(
+		"CONDITION" => "#^/personal/order/#",
+		"RULE" => "",
+		"ID" => "bitrix:sale.personal.order",
+		"PATH" => "/personal/order/index.php",
+	),
+	array(
+		"CONDITION" => "#^/personal/#",
+		"RULE" => "",
+		"ID" => "bitrix:sale.personal.section",
+		"PATH" => "/personal/index.php",
+	),
+	array(
+		"CONDITION" => "#^/catalog/#",
+		"RULE" => "",
+		"ID" => "bitrix:catalog",
+		"PATH" => "/catalog/index.php",
+	),
+	array(
+		"CONDITION" => "#^/store/#",
+		"RULE" => "",
+		"ID" => "bitrix:catalog.store",
+		"PATH" => "/store/index.php",
+	),
+	array(
+		"CONDITION" => "#^/news/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/news/index.php",
+	),
+);
+
+?>
